4054ddb5cfbd188933f63827f53b1e83bfdc3488
Eric Internicola <einternicola@silverchalice.com>
2015-07-19 20:27:29 -0600

    Added the layer-parse-module, then updated the key in that file (from the parse site: https://developer.layer.com/projects/chatter/keys) and deployed it successfully.

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,16 @@
+var fs = require('fs');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var layerProviderID = 'layer:///providers/e0096bde-2d6c-11e5-903b-b6da1c001f34';
+var layerKeyID = 'layer:///keys/d6799b90-2e82-11e5-b6c0-4290000000fb';
+var privateKey = fs.readFileSync('cloud/layer-parse-module/keys/layer-key.js');
+
+var layer = require('cloud/layer-parse-module/layer-module.js');
+layer.initialize(layerProviderID, layerKeyID, privateKey);
+
+Parse.Cloud.define("generateToken", function(request, response) {
+    var userID = request.params.userID;
+    var nonce = request.params.nonce;
+    if (!userID) throw new Error('Missing userID parameter');
+    if (!nonce) throw new Error('Missing nonce parameter');
+        response.success(layer.layerIdentityToken(userID, nonce));
 });
